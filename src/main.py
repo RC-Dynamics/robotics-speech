f562044e4c37cd412920ff220034915bb73c0a2a
wmr <wmr@cin.ufpe.br>
2019-12-01 18:10:24 -0300

    added grammar and example

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import speech_recognition as sr
+import nltk
+nltk.download('punkt')
 
-
-
-sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
-grammar = nltk.data.load('src/athome.cfg')
+grammar = nltk.data.load('./grammar/athome.cfg')
+sent = 'pick up the blue cup'
+sent = nltk.word_tokenize(sent)
 rd_parser = nltk.RecursiveDescentParser(grammar)
 trees = rd_parser.parse(sent)
+for t in trees:
+    print(t)
