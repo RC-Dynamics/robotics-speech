048af9ce73d614ce9fa4af77e43f36a4ff33e996
Roberto <betocostaf@gmail.com>
2019-12-01 17:07:29 -0300

    Organazing files
    Co-authored-by: Walber de Macedo Rodrigues <wmr@cin.ufpe.br>

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
 import speech_recognition as sr
+
+
+
+sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
+grammar = nltk.data.load('src/athome.cfg')
+rd_parser = nltk.RecursiveDescentParser(grammar)
+trees = rd_parser.parse(sent)
