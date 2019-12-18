2c904170335708324a7248273a8c2c820ccbf0dc
walbermr <wmr@cin.ufpe.br>
2019-12-18 09:31:14 -0300

    Added record sample script updated requirements

---
+++ src/record_samples.py
@@ -0,0 +1,130 @@
+import nltk
+import os
+import sys
+import pyaudio
+import wave
+import numpy as np
+
+from nltk.parse.generate import generate
+
+
+np.random.seed(199)
+
+class Recorder(object):
+    '''A recorder class for recording audio to a WAV file.
+    Records in mono by default.
+    '''
+
+    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024):
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+
+    def open(self, fname, mode='wb'):
+        return RecordingFile(fname, mode, self.channels, self.rate,
+                            self.frames_per_buffer)
+
+class RecordingFile(object):
+    def __init__(self, fname, mode, channels, 
+                rate, frames_per_buffer):
+        self.fname = fname
+        self.mode = mode
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+        self._pa = pyaudio.PyAudio()
+        self.wavefile = self._prepare_file(self.fname, self.mode)
+        self._stream = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception, value, traceback):
+        self.close()
+
+    def record(self, duration):
+        # Use a stream with no callback function in blocking mode
+        self._stream = self._pa.open(format=pyaudio.paInt16,
+                                        channels=self.channels,
+                                        rate=self.rate,
+                                        input=True,
+                                        frames_per_buffer=self.frames_per_buffer)
+        for _ in range(int(self.rate / self.frames_per_buffer * duration)):
+            audio = self._stream.read(self.frames_per_buffer)
+            self.wavefile.writeframes(audio)
+        return None
+
+    def start_recording(self):
+        # Use a stream with a callback in non-blocking mode
+        self._stream = self._pa.open(format=pyaudio.paInt16,
+                                        channels=self.channels,
+                                        rate=self.rate,
+                                        input=True,
+                                        frames_per_buffer=self.frames_per_buffer,
+                                        stream_callback=self.get_callback())
+        self._stream.start_stream()
+        return self
+
+    def stop_recording(self):
+        self._stream.stop_stream()
+        return self
+
+    def get_callback(self):
+        def callback(in_data, frame_count, time_info, status):
+            self.wavefile.writeframes(in_data)
+            return in_data, pyaudio.paContinue
+        return callback
+
+
+    def close(self):
+        self._stream.close()
+        self._pa.terminate()
+        self.wavefile.close()
+
+    def _prepare_file(self, fname, mode='wb'):
+        wavefile = wave.open(fname, mode)
+        wavefile.setnchannels(self.channels)
+        wavefile.setsampwidth(self._pa.get_sample_size(pyaudio.paInt16))
+        wavefile.setframerate(self.rate)
+        return wavefile
+
+
+def generate_phrases(grammar):
+    phrases = []
+    for sentence in generate(grammar, depth=10):
+        phrases.append(' '.join(sentence))
+
+    return phrases
+
+if __name__ == '__main__':
+    grammar = nltk.data.load('./grammar/athome.cfg')
+    phrases = generate_phrases(grammar)
+    phrases = list(np.random.permutation(phrases))
+    rec = Recorder(channels=1)
+    os.system('clear')
+
+    try:
+        with open('audio_samples/checkpoint.in', 'r') as checkpoint:
+            i = int(checkpoint.readline())
+    except:
+        i = 0
+
+
+    while i < len(phrases):
+        phrase = phrases[i]
+        
+        with rec.open('audio_samples/%s.wav' %(phrase), 'wb') as recfile:
+            os.system('clear')
+            input('Press enter to start recording and say: %s' %(phrase))
+            recfile.start_recording()
+
+            print('Please say " %s ".' %(phrase))
+            input('Press enter to stop recording...')
+            recfile.stop_recording()
+
+            os.system('clear')
+
+        i += 1
+        with open('audio_samples/checkpoint.in', 'w') as checkpoint:
+            checkpoint.write(str(i))
+    
